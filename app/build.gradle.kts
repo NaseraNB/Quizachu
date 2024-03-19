593d4ac6bd7c362f2df0f17f09a0204317c9bfea
NaseraNB <04nasera.b@inscastellet.cat>
2024-03-19 17:04:35 +0100

    Login con google, funciona

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,16 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Google
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 }
--- app/src/main/java/com/example/quizachu/Login.kt
+++ app/src/main/java/com/example/quizachu/Login.kt
@@ -1,14 +1,27 @@
 package com.example.quizachu
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import com.google.rpc.context.AttributeContext.Auth
+import java.util.Objects
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class Login : AppCompatActivity() {
     //Despleguem les variables que farem servir
@@ -16,6 +29,12 @@ class Login : AppCompatActivity() {
     lateinit var passLogin : EditText
     lateinit var BtnLogin : Button
     lateinit var auth: FirebaseAuth //FIREBASE AUTENTIFICACIO
+    lateinit var BtnGoogle : Button
+    lateinit var db: FirebaseDatabase
+    lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 123 // Asigna el valor que desees
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +45,9 @@ class Login : AppCompatActivity() {
         correoLogin =findViewById<EditText>(R.id.correoLogin)
         passLogin =findViewById<EditText>(R.id.passLogin)
         BtnLogin =findViewById<Button>(R.id.BtnLogin)
+        BtnGoogle =findViewById<Button>(R.id.googleButton)
+
+
 
         BtnLogin.setOnClickListener(){
             //Abans de fer el registre validem les dades
@@ -45,11 +67,72 @@ class Login : AppCompatActivity() {
             }
         }
 
+
         //Instanciem el firebaseAuth
         auth = FirebaseAuth.getInstance()
+        db = FirebaseDatabase.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
 
+        BtnGoogle.setOnClickListener {
+            googleSignIn()
+        }
 
     }
+
+    private fun googleSignIn() {
+        val intent = googleSignInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            handleGoogleSignInResult(data)
+        }
+    }
+
+    private fun handleGoogleSignInResult(data: Intent?) {
+        val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            firebaseAuth(account?.idToken)
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+    private fun firebaseAuth(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val map = HashMap<String, Any>()
+                    map["id"] = user?.uid ?: ""
+                    map["name"] = user?.displayName ?: ""
+
+                    db.reference.child("users").child(user?.uid ?: "").setValue(map)
+                        .addOnCompleteListener { innerTask ->
+                            if (innerTask.isSuccessful) {
+                                val intent = Intent(this@Login, Menu::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(this@Login, "Something went wrong", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this@Login, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
 
     private fun LogindeJugador(email: String, passw: String) {
         auth.signInWithEmailAndPassword(email, passw)
