d35039a0689300adcc580fd7f7ee2a384c7f9bc6
Yarií Soto <yariisoto@proton.me>
2024-03-24 19:45:07 +0100

    Muestra los Pokémons correctamente, está preparado para generar tres niveles de dificultad

--- app/src/main/java/com/example/quizachu/models/PokeInfoViewModel.kt
+++ app/src/main/java/com/example/quizachu/models/PokeInfoViewModel.kt
@@ -1,17 +1,19 @@
 package com.example.quizachu.models
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.quizachu.pokeApi.ApiService
 import com.example.quizachu.pokeApi.PokeApiResponse
 import com.example.quizachu.pokeApi.Pokemon
+import com.example.quizachu.pokeApi.SingleLiveEvent
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class PokeInfoViewModel() : ViewModel() {
+class PokeInfoViewModel : ViewModel() {
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://pokeapi.co/api/v2/")
         .addConverterFactory(GsonConverterFactory.create())
@@ -19,12 +21,12 @@ class PokeInfoViewModel() : ViewModel() {
 
     private val service: ApiService = retrofit.create(ApiService::class.java)
 
-    //Obtenir dades del Pokemon
-    val pokemonInfo = MutableLiveData<List<Pokemon>>()
+    //La variable pokemonInfo amb un SingleLiveEven per enviar actualitzacions només una vegada, així no es dupliquen els pokémons
+    val pokemonInfo = SingleLiveEvent<List<Pokemon>>()
+    val errorMessage = MutableLiveData<String>()
 
-    //Mètode per aconseguir una llista aleatòria de tres Pokémons
     fun getPokemonListAndInfo() {
-        val randomOffset = (0..898).random() //Generar un offset aleatori
+        val randomOffset = (0..898).random() // Generar un offset aleatorio
 
         val call = service.getPokemonList(limit = 3, offset = randomOffset)
         call.enqueue(object : Callback<PokeApiResponse> {
@@ -33,34 +35,36 @@ class PokeInfoViewModel() : ViewModel() {
                     val pokeApiResponse = response.body()
                     val pokemonList = mutableListOf<Pokemon>()
                     pokeApiResponse?.results?.forEachIndexed { index, pokeResult ->
-                        val pokemonId = randomOffset + index + 1 //id de Pokémon basat en l'offset i l'índex
+                        val pokemonId = randomOffset + index + 1 // id de Pokémon basado en el offset y el índice
                         val pokemonInfoCall = service.getPokemonInfo(pokemonId)
                         pokemonInfoCall.enqueue(object : Callback<Pokemon> {
                             override fun onResponse(call: Call<Pokemon>, response: Response<Pokemon>) {
                                 if (response.isSuccessful) {
-                                    val pokemon = response.body()
-                                    pokemon?.let { pokemonList.add(it) }
+                                    response.body()?.let { pokemonList.add(it) }
                                     if (pokemonList.size == 3) {
                                         pokemonInfo.postValue(pokemonList)
                                     }
                                 } else {
-                                    //No se que poner
-                                    //TODO
+                                    Log.e("PokeInfoViewModel", "Error al obtener información del Pokémon: ${response.errorBody()?.string()}")
+                                    errorMessage.postValue("Error al obtener información del Pokémon.")
                                 }
                             }
 
                             override fun onFailure(call: Call<Pokemon>, t: Throwable) {
-                                //TODO
+                                Log.e("PokeInfoViewModel", "Fallo al realizar la llamada a la API para obtener información del Pokémon", t)
+                                errorMessage.postValue("Fallo al realizar la llamada a la API para obtener información del Pokémon.")
                             }
                         })
                     }
                 } else {
-                    //TODO
+                    Log.e("PokeInfoViewModel", "Error al obtener la lista de Pokémon: ${response.errorBody()?.string()}")
+                    errorMessage.postValue("Error al obtener la lista de Pokémon.")
                 }
             }
 
             override fun onFailure(call: Call<PokeApiResponse>, t: Throwable) {
-               //TODO
+                Log.e("PokeInfoViewModel", "Fallo al realizar la llamada a la API para obtener la lista de Pokémon", t)
+                errorMessage.postValue("Fallo al realizar la llamada a la API para obtener la lista de Pokémon.")
             }
         })
     }
--- app/src/main/java/com/example/quizachu/ui/PokeGameScreen.kt
+++ app/src/main/java/com/example/quizachu/ui/PokeGameScreen.kt
@@ -1,143 +1,203 @@
 package com.example.quizachu.ui
 
 import android.graphics.Bitmap
-import android.widget.ImageView
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
-import java.lang.Exception
-
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.quizachu.R
 import com.example.quizachu.models.PokeInfoViewModel
 import com.example.quizachu.pokeApi.Pokemon
-import kotlin.random.Random
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.*
+
 
 class PokeGameScreen : AppCompatActivity() {
+    companion object {
+        const val MAX_QUESTIONS_PER_LEVEL = 5
+        const val NUM_LEVELS = 3
+    }
+
+    //Variables d'iniciació
     private lateinit var viewModel: PokeInfoViewModel
-    private var currentPokemonName: String? = null
-    private lateinit var pokemonSelect: Pokemon
-    private var buttonClick = false
+    private lateinit var imageView: ImageView
+    private lateinit var buttons: List<Button>
+    private var numQuestionsAsked = 0
+    private var gameJob: Job? = null
+    private lateinit var pokemonList: List<Pokemon>
+    private lateinit var selectedPokemon: Pokemon
+    private var currentLevel = 1
+    private var score = 0
 
-    //Generar un nombre aleatori entre 0 i 2 per escollir el pokémon correcte
-    private var randomNumber: Int = 0
-
+    //Mètodes onCreate y initUI
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poke_game_screen)
 
-        //Crear instància de PokeInfoViewModel
-        viewModel = ViewModelProvider(this).get(PokeInfoViewModel::class.java)
-
-        //Generar un nombre aleatori entre 0 i 2 per escollir el pokémon correcte
-        randomNumber = Random.nextInt(3)
-
-        //Obtenir la llista de Pokémons
-        viewModel.getPokemonListAndInfo()
-
+        imageView = findViewById(R.id.imagePokemon)
+        buttons = listOf(
+            findViewById(R.id.btnPokemon1),
+            findViewById(R.id.btnPokemon2),
+            findViewById(R.id.btnPokemon3)
+        )
+        viewModel = ViewModelProvider(this)[PokeInfoViewModel::class.java]
         initUI()
     }
 
     private fun initUI() {
-        //Guardar els botons
-        val buttons = listOf<Button>(findViewById(R.id.btnPokemon1), findViewById(R.id.btnPokemon2), findViewById(R.id.btnPokemon3))
+        askQuestion()
+    }
 
-        viewModel.pokemonInfo.observe(this, Observer { pokemonList ->
+    private fun askQuestion() {
+        //Sol·licitar llista de pokémons
+        viewModel.getPokemonListAndInfo()
+        //Observar la variable pokemonInfo i obtenir les dades
+        viewModel.pokemonInfo.observe(this) { newList ->
+            if (newList.isNotEmpty()) {
+                //Guardar la llista a una variable global per ferla servir a tota la classe
+                pokemonList = newList
 
-            //Assignar els noms dels Pokémons als botons
-            pokemonList.forEachIndexed { index, pokemon ->
-                buttons[index].text = pokemon.name
+                //Seleccionar un pokémon aleatori
+                selectedPokemon = pokemonList.random()
+
+                //Log.d("PokeGame", "Pokémon seleccionado: ${selectedPokemon.name}, Lista completa: $pokemonList")
+
+                //Carregar la imatge del pokémon seleccionat
+                loadPokemonImage()
+
+                //Actualitzar els botons amb els noms dels pokémons
+                updateButtonNames()
             }
+        }
+    }
 
-            pokemonSelect = pokemonList[randomNumber]
-            //Carregar la imatge del Pokémon
-            loadPokemonImage(pokemonSelect)
+    private fun updateButtonNames() {
+        //Assegurar-se que hi ha suficients noms pels botons
+        if (pokemonList.size >= buttons.size) {
+            buttons.forEachIndexed { index, button ->
+                button.text = pokemonList[index].name
+                button.setOnClickListener {
+                    handleButtonClick(button.text.toString())
+                }
+            }
+        }
+    }
+    private fun handleButtonClick(clickedPokemonName: String) {
+        Picasso.get().load(selectedPokemon.sprites.frontDefault).into(imageView)
+
+        // Actualiza la puntuación y decide si cargar la siguiente pregunta
+        updateScore(clickedPokemonName == selectedPokemon.name)
+        if (numQuestionsAsked < MAX_QUESTIONS_PER_LEVEL) {
+            // Espera y carga la siguiente pregunta
+            prepareNextQuestion()
+        } else {
+            // Finaliza el nivel
+            endRound()
+        }
+    }
+    private fun loadPokemonImage() {
+        Picasso.get().load(selectedPokemon.sprites.frontDefault).into(imageView, object : Callback {
+            override fun onSuccess() {
+                applyImageFilter()
+            }
+            override fun onError(e: Exception?) {
+                Toast.makeText(this@PokeGameScreen, "Error al carregar la imatge.", Toast.LENGTH_SHORT).show()
+            }
         })
-
-        buttons.forEach { button ->
-            button.setOnClickListener {
-                //Guardem el valor del botó on ha fet clic l'usuari
-                val clickedPokemonName = button.text.toString()
-                buttonClick = true
-
-
-                if (currentPokemonName == clickedPokemonName) {
-                    button.setBackgroundColor(Color.GREEN)
-                    Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show()
-
-                } else {
-                    button.setBackgroundColor(Color.RED)
-                    val correctPokemonName = currentPokemonName ?: ""
-                    Toast.makeText(this, "¡Incorrecto! El Pokémon es $correctPokemonName", Toast.LENGTH_SHORT).show()
-                }
-
-                loadPokemonImage(pokemonSelect)
-            }
-        }
     }
+    private fun applyImageFilter(){
+        val levels = intArrayOf(0,2,5)
+        val drawable = imageView.drawable as BitmapDrawable
+        val bitmap = drawable.bitmap
 
-    //Mètode per mostrar la imatge del pokémon amb Picasso
-    private fun loadPokemonImage(pokemon: Pokemon) {
-        currentPokemonName = pokemon.name
+        val resultBitmap = if(currentLevel>1){
+            pixelateImage(applyBacklitFilter(bitmap), pixelSize = levels[currentLevel-1])
 
-        // Buscar el ImageView donde se mostrará la imagen del Pokémon
-        val imageView = findViewById<ImageView>(R.id.imagePokemon)
-
-        // Crear una instancia de Picasso y cargar la imagen del Pokémon
-        Picasso.get().load(pokemon.sprites.frontDefault)
-            //.placeholder(R.drawable.placeholder_image) // Imagen de carga predeterminada
-            //.error(R.drawable.error_image) // Imagen de error
-            .into(imageView, object : Callback {
-                override fun onSuccess() {
-                    if(!buttonClick){
-                        // Cuando la carga de la imagen sea exitosa, aplicar el filtro de contraluz
-                        applyBacklitFilter(imageView)
-                    }
-
-                }
-
-                override fun onError(e: Exception?) {
-                    // Manejar el caso de error, si lo hay
-                }
-            })
-    }
-
-    //Mètode per aplicar un filtre negre al pokémon
-    private fun applyBacklitFilter(imageView: ImageView) {
-        //Obtenir la imatge del ImageView
-        val drawable = imageView.drawable
-
-        //Convertir la imatge a Bitmap
-        val bitmap = (drawable as BitmapDrawable).bitmap
-
-        //Crear un nou Bitmap per guardar el resultat
-        val resultBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-
-        //Iterar sobre tots els  píxeles de la imatge
-        for (x in 0 until bitmap.width) {
-            for (y in 0 until bitmap.height) {
-                //Obtenir el color del píxel a la posició (x,y)
-                val pixelColor = bitmap.getPixel(x, y)
-
-                //Verificar si el píxel no és transparent
-                if (Color.alpha(pixelColor) != 0) {
-                    //Si el píxel no és transparent, es posa en negre
-                    resultBitmap.setPixel(x, y, Color.BLACK)
-                } else {
-                    //Si el píxel és transparent, deixar-lo transparent
-                    resultBitmap.setPixel(x, y, Color.TRANSPARENT)
-                }
-            }
+        }else{
+            applyBacklitFilter(bitmap)
         }
 
-        //Posar el Bitmap resultant al ImageView
+        //Es carrega a la imageView el resultat del bitmap
         imageView.setImageBitmap(resultBitmap)
     }
 
+    //Mètode per crear un bitmap amb els bits en negre
+    private fun applyBacklitFilter(bitmap: Bitmap): Bitmap {
+        val resultBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+
+        //S'agafa cada bit i es posa en negre, en cas que sigui transparent es posa transparent
+        for (x in 0 until bitmap.width) {
+            for (y in 0 until bitmap.height) {
+                val pixelColor = bitmap.getPixel(x, y)
+                resultBitmap.setPixel(
+                    x, y,
+                    if (Color.alpha(pixelColor) != 0) Color.BLACK else Color.TRANSPARENT
+                )
+            }
+        }
+        return resultBitmap
+    }
+
+    private fun pixelateImage(bitmap: Bitmap, pixelSize: Int): Bitmap {
+        val width = bitmap.width
+        val height = bitmap.height
+        val pixelated = Bitmap.createBitmap(width, height, bitmap.config)
+
+        for (x in 0 until width step pixelSize) {
+            for (y in 0 until height step pixelSize) {
+                val pixel = bitmap.getPixel(x, y)
+                for (dx in x until (x + pixelSize)) {
+                    for (dy in y until (y + pixelSize)) {
+                        if (dx < width && dy < height) {
+                            pixelated.setPixel(dx, dy, pixel)
+                        }
+                    }
+                }
+            }
+        }
+
+        return pixelated
+    }
+
+    private fun prepareNextQuestion() {
+        //Es verifica si hi ha alguna tasca en execució i la cancel·la per evitar errors
+        gameJob?.cancel()
+        //Crea una nova tasca per cridar la següent pregunta
+        gameJob = CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            askQuestion()
+        }
+    }
+
+    private fun updateScore(isCorrect: Boolean) {
+        val pokeCorrect = selectedPokemon.name
+        if (isCorrect) {
+            score++
+            Toast.makeText(this, "¡Correcto!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "¡Incorrecto! El Pokémon es $pokeCorrect", Toast.LENGTH_SHORT).show()
+        }
+
+        numQuestionsAsked++
+        if (numQuestionsAsked == MAX_QUESTIONS_PER_LEVEL) {
+            endRound()
+        }
+    }
+    private fun endRound() {
+        // Mostrar la puntuación y permitir al usuario volver a la pantalla de niveles
+        showScoreScreen()
+        currentLevel++
+        if (currentLevel > NUM_LEVELS) {
+            // Fin del juego, mostrar resultados finales o reiniciar
+        }
+    }
+
+    private fun showScoreScreen() {
+        // Lógica para mostrar la pantalla de puntuación
+    }
 }
