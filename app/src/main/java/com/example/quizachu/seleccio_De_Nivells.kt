7a3a2a42df2a1bc441290dc8a4b622aa784ceb62
NaseraNB <04nasera.b@inscastellet.cat>
2024-03-25 12:56:30 +0100

    recuperar la contrasenya finalitzat

---
+++ app/src/main/java/com/example/quizachu/seleccio_De_Nivells.kt
@@ -0,0 +1,11 @@
+package com.example.quizachu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class seleccio_De_Nivells : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccio_de_nivells)
+    }
+}
