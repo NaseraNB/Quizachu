d8491c9d6c911dff5af10df8ff9e4fdabc15ebc8
Yarií Soto Pereira <yariisoto@proton.me>
2024-04-04 17:54:11 +0200

    Revert "Game"

--- app/src/main/java/com/example/quizachu/pokeApi/PokeApiResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quizachu.pokeApi
-
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-//Classe per la resposta de l'API
-data class PokeApiResponse(
-    @SerializedName("results") val results: List<Pokemon>
-)
